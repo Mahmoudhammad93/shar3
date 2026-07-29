5cd72fa5c275b6f2e545f8bf6cf55d4581e8e599
Mahmoud Hammad <mahmoudhammad423@gmail.com>
2026-07-29 16:54:58 +0300

    Deploy quiz question types UI and latest student portal updates

---
+++ _next/static/0hieZKJT7hd8oxZENo4DO/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcourses\u002F[slug]","\u002Fdashboard\u002Fcourses\u002F[id]","\u002Fnews\u002F[slug]","\u002Fprograms\u002F[slug]","\u002Fteachers\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
